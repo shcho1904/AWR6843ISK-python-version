eb4b9a3a6fe9b3b453fc95ba0a72278c2c9c51fe
Sungho Cho <39304933+shcho1904@users.noreply.github.com>
2025-01-25 15:12:53 -0800

    Upload all files

---
+++ mmw_parse_script.py
@@ -0,0 +1,381 @@
+# ****************************************************************************
+# * (C) Copyright 2020, Texas Instruments Incorporated. - www.ti.com
+# ****************************************************************************
+# *
+# *  Redistribution and use in source and binary forms, with or without
+# *  modification, are permitted provided that the following conditions are
+# *  met:
+# *
+# *    Redistributions of source code must retain the above copyright notice,
+# *    this list of conditions and the following disclaimer.
+# *
+# *    Redistributions in binary form must reproduce the above copyright
+# *    notice, this list of conditions and the following disclaimer in the
+# *     documentation and/or other materials provided with the distribution.
+# *
+# *    Neither the name of Texas Instruments Incorporated nor the names of its
+# *    contributors may be used to endorse or promote products derived from
+# *    this software without specific prior written permission.
+# *
+# *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# *  PARTICULAR TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# *  A PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  OWNER OR
+# *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# *  EXEMPLARY, ORCONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# *  LIABILITY, WHETHER IN CONTRACT,  STRICT LIABILITY, OR TORT (INCLUDING
+# *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# *
+# ****************************************************************************
+
+
+# ****************************************************************************
+# Sample mmW demo UART output parser script - should be invoked using python3
+#       ex: python3 mmw_demo_example_script.py <recorded_dat_file_from_Visualizer>.dat
+#
+# Notes:
+#   1. The parser_mmw_demo script will output the text version 
+#      of the captured files on stdio. User can redirect that output to a log file, if desired
+#   2. This example script also outputs the detected point cloud data in mmw_demo_output.csv 
+#      to showcase how to use the output of parser_one_mmw_demo_output_packet
+# ****************************************************************************
+import serial
+import time
+import numpy as np
+import os
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+import pyqtgraph as pg
+# import the parser function 
+from parser_mmw_demo import parser_one_mmw_demo_output_packet
+
+# Change the configuration file name
+configFileName = 'xwr68xxconfig.cfg'
+
+# Change the debug variable to use print()
+DEBUG = False
+
+# Constants
+maxBufferSize = 2**15;
+CLIport = {}
+Dataport = {}
+byteBuffer = np.zeros(2**15,dtype = 'uint8')
+byteBufferLength = 0;
+maxBufferSize = 2**15;
+magicWord = [2, 1, 4, 3, 6, 5, 8, 7]
+detObj = {}  
+frameData = {}    
+currentIndex = 0
+# word array to convert 4 bytes to a 32 bit number
+word = [1, 2**8, 2**16, 2**24]
+
+# Function to configure the serial ports and send the data from
+# the configuration file to the radar
+def serialConfig(configFileName):
+    
+    global CLIport
+    global Dataport
+    # Open the serial ports for the configuration and the data ports
+    
+    # Raspberry pi
+    #CLIport = serial.Serial('/dev/ttyACM0', 115200)
+    #Dataport = serial.Serial('/dev/ttyACM1', 921600)
+    
+    # Windows
+    CLIport = serial.Serial('COM12', 115200)
+    Dataport = serial.Serial('COM11', 921600)
+
+    # Read the configuration file and send it to the board
+    config = [line.rstrip('\r\n') for line in open(configFileName)]
+    for i in config:
+        CLIport.write((i+'\n').encode())
+        print(i)
+        time.sleep(0.01)
+        
+    return CLIport, Dataport
+
+# Function to parse the data inside the configuration file
+def parseConfigFile(configFileName):
+    configParameters = {} # Initialize an empty dictionary to store the configuration parameters
+    
+    # Read the configuration file and send it to the board
+    config = [line.rstrip('\r\n') for line in open(configFileName)]
+    for i in config:
+        
+        # Split the line
+        splitWords = i.split(" ")
+        
+        # Hard code the number of antennas, change if other configuration is used
+        numRxAnt = 4
+        numTxAnt = 3
+        
+        # Get the information about the profile configuration
+        if "profileCfg" in splitWords[0]:
+            startFreq = int(float(splitWords[2]))
+            idleTime = int(splitWords[3])
+            rampEndTime = float(splitWords[5])
+            freqSlopeConst = float(splitWords[8])
+            numAdcSamples = int(splitWords[10])
+            numAdcSamplesRoundTo2 = 1;
+            
+            while numAdcSamples > numAdcSamplesRoundTo2:
+                numAdcSamplesRoundTo2 = numAdcSamplesRoundTo2 * 2;
+                
+            digOutSampleRate = int(splitWords[11]);
+            
+        # Get the information about the frame configuration    
+        elif "frameCfg" in splitWords[0]:
+            
+            chirpStartIdx = int(splitWords[1]);
+            chirpEndIdx = int(splitWords[2]);
+            numLoops = int(splitWords[3]);
+            numFrames = int(splitWords[4]);
+            framePeriodicity = int(splitWords[5]);
+
+            
+    # Combine the read data to obtain the configuration parameters           
+    numChirpsPerFrame = (chirpEndIdx - chirpStartIdx + 1) * numLoops
+    configParameters["numDopplerBins"] = numChirpsPerFrame / numTxAnt
+    configParameters["numRangeBins"] = numAdcSamplesRoundTo2
+    configParameters["rangeResolutionMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * numAdcSamples)
+    configParameters["rangeIdxToMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * configParameters["numRangeBins"])
+    configParameters["dopplerResolutionMps"] = 3e8 / (2 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * configParameters["numDopplerBins"] * numTxAnt)
+    configParameters["maxRange"] = (300 * 0.9 * digOutSampleRate)/(2 * freqSlopeConst * 1e3)
+    configParameters["maxVelocity"] = 3e8 / (4 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * numTxAnt)
+    
+    return configParameters
+
+##################################################################################
+# USE parser_mmw_demo SCRIPT TO PARSE ABOVE INPUT FILES
+##################################################################################
+def readAndParseData18xx(Dataport, configParameters):
+    global byteBuffer, byteBufferLength
+    
+    # Constants
+    OBJ_STRUCT_SIZE_BYTES = 12
+    BYTE_VEC_ACC_MAX_SIZE = 2**15
+    MMWDEMO_UART_MSG_DETECTED_POINTS = 1
+    MMWDEMO_UART_MSG_RANGE_PROFILE   = 2
+    maxBufferSize = 2**15
+    tlvHeaderLengthInBytes = 8
+    pointLengthInBytes = 16
+    magicWord = [2, 1, 4, 3, 6, 5, 8, 7]
+    
+    # Initialize variables
+    magicOK = 0 # Checks if magic number has been read
+    dataOK = 0 # Checks if the data has been read correctly
+    frameNumber = 0
+    detObj = {}
+    
+    readBuffer = Dataport.read(Dataport.in_waiting)
+    byteVec = np.frombuffer(readBuffer, dtype = 'uint8')
+    byteCount = len(byteVec)
+    
+    # Check that the buffer is not full, and then add the data to the buffer
+    if (byteBufferLength + byteCount) < maxBufferSize:
+        byteBuffer[byteBufferLength:byteBufferLength + byteCount] = byteVec[:byteCount]
+        byteBufferLength = byteBufferLength + byteCount
+        
+    # Check that the buffer has some data
+    if byteBufferLength > 16:
+        
+        # Check for all possible locations of the magic word
+        possibleLocs = np.where(byteBuffer == magicWord[0])[0]
+
+        # Confirm that is the beginning of the magic word and store the index in startIdx
+        startIdx = []
+        for loc in possibleLocs:
+            check = byteBuffer[loc:loc+8]
+            if np.all(check == magicWord):
+                startIdx.append(loc)
+               
+        # Check that startIdx is not empty
+        if startIdx:
+            
+            # Remove the data before the first start index
+            if startIdx[0] > 0 and startIdx[0] < byteBufferLength:
+                byteBuffer[:byteBufferLength-startIdx[0]] = byteBuffer[startIdx[0]:byteBufferLength]
+                byteBuffer[byteBufferLength-startIdx[0]:] = np.zeros(len(byteBuffer[byteBufferLength-startIdx[0]:]),dtype = 'uint8')
+                byteBufferLength = byteBufferLength - startIdx[0]
+                
+            # Check that there have no errors with the byte buffer length
+            if byteBufferLength < 0:
+                byteBufferLength = 0
+                
+            # word array to convert 4 bytes to a 32 bit number
+            word = [1, 2**8, 2**16, 2**24]
+            
+            # Read the total packet length
+            totalPacketLen = np.matmul(byteBuffer[12:12+4],word)
+            
+            # Check that all the packet has been read
+            if (byteBufferLength >= totalPacketLen) and (byteBufferLength != 0):
+                magicOK = 1
+    
+    # If magicOK is equal to 1 then process the message
+    if magicOK:
+        # word array to convert 4 bytes to a 32 bit number
+        word = [1, 2**8, 2**16, 2**24]
+        
+        # Initialize the pointer index
+        idX = 0
+        
+        # Read the header
+        magicNumber = byteBuffer[idX:idX+8]
+        idX += 8
+        version = format(np.matmul(byteBuffer[idX:idX+4],word),'x')
+        idX += 4
+        totalPacketLen = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        platform = format(np.matmul(byteBuffer[idX:idX+4],word),'x')
+        idX += 4
+        frameNumber = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        timeCpuCycles = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        numDetectedObj = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        numTLVs = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        subFrameNumber = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+
+        # Read the TLV messages
+        for tlvIdx in range(numTLVs):
+            
+            # word array to convert 4 bytes to a 32 bit number
+            word = [1, 2**8, 2**16, 2**24]
+
+            # Check the header of the TLV message
+            tlv_type = np.matmul(byteBuffer[idX:idX+4],word)
+            idX += 4
+            tlv_length = np.matmul(byteBuffer[idX:idX+4],word)
+            idX += 4
+
+            # Read the data depending on the TLV message
+            if tlv_type == MMWDEMO_UART_MSG_DETECTED_POINTS:
+
+                # Initialize the arrays
+                x = np.zeros(numDetectedObj,dtype=np.float32)
+                y = np.zeros(numDetectedObj,dtype=np.float32)
+                z = np.zeros(numDetectedObj,dtype=np.float32)
+                velocity = np.zeros(numDetectedObj,dtype=np.float32)
+                
+                for objectNum in range(numDetectedObj):
+                    
+                    # Read the data for each object
+                    x[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32).item()
+                    idX += 4
+                    y[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32).item()
+                    idX += 4
+                    z[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32).item()
+                    idX += 4
+                    velocity[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32).item()
+                    idX += 4
+                
+                # Store the data in the detObj dictionary
+                detObj = {"numObj": numDetectedObj, "x": x, "y": y, "z": z, "velocity":velocity}
+                dataOK = 1
+                
+ 
+        # Remove already processed data
+        if idX > 0 and byteBufferLength>idX:
+            shiftSize = totalPacketLen
+            
+                
+            byteBuffer[:byteBufferLength - shiftSize] = byteBuffer[shiftSize:byteBufferLength]
+            byteBuffer[byteBufferLength - shiftSize:] = np.zeros(len(byteBuffer[byteBufferLength - shiftSize:]),dtype = 'uint8')
+            byteBufferLength = byteBufferLength - shiftSize
+            
+            # Check that there are no errors with the buffer length
+            if byteBufferLength < 0:
+                byteBufferLength = 0         
+
+    return dataOK, frameNumber, detObj
+
+
+class MyWidget(pg.GraphicsLayoutWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.mainLayout = QVBoxLayout()
+        self.setLayout(self.mainLayout)
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(100) # in milliseconds
+        self.timer.start()
+        self.timer.timeout.connect(self.onNewData)
+
+        self.plotItem = self.addPlot(title="Ladar points")
+
+        self.plotDataItem = self.plotItem.plot([], pen=None, 
+            symbolBrush=(255,0,0), symbolSize=5, symbolPen=None)
+
+
+    def setData(self, x, y):
+        self.plotDataItem.setData(x, y)
+
+    # Funtion to update the data and display in the plot
+    def update(self):
+        
+        dataOk = 0
+        global detObj
+        x = []
+        y = []
+        
+        # Read and parse the received data
+        dataOk, frameNumber, detObj = readAndParseData18xx(Dataport, configParameters)
+        if dataOk and len(detObj["x"]) > 0:
+            #print(detObj)
+            x = detObj["x"]
+            y = detObj["y"]
+            # print(x)
+
+        return dataOk, x, y
+
+
+    def onNewData(self):
+        
+        # Update the data and check if the data is okay        
+        dataOk,newx,newy = self.update()
+
+        #if dataOk:
+            # Store the current frame into frameData
+            #frameData[currentIndex] = detObj
+            #currentIndex += 1
+        
+        x = newx
+        y = newy
+        self.setData(x, y)
+
+
+def main():        
+    # Configurate the serial port
+    CLIport, Dataport = serialConfig(configFileName)
+
+    # Get the configuration parameters from the configuration file
+    global configParameters 
+    configParameters = parseConfigFile(configFileName)
+
+    app = QApplication([])
+
+    pg.setConfigOptions(antialias=False) # True seems to work as well
+
+    win = MyWidget()
+    win.show()
+    win.resize(800,600) 
+    win.raise_()
+    app.exec_()
+    CLIport.write(('sensorStop\n').encode())
+    CLIport.close()
+    Dataport.close()
+
+
+
+if __name__ == "__main__":
+    main()
